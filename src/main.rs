2fa0ebd8cf6aa6f7495db4b9405b2ac4ab8bc0b2
SwarnabhR <swarnabh.roy@gmail.com>
2024-08-23 02:38:42 +0530

    Commit new data structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
 struct Stack<T> {
     items: Vec<T>,
 }
+
+impl<T> Stack<T> {
+    fn new() -> Self {
+        Stack { items: Vec::new() }
+    }
+
+    fn push(&mut self, item: T) {
+        self.items.push(item);
+    }
+
+    fn pop(&mut self) -> Option<T> {
+        self.items.pop()
+    }
+
+    fn peek(&self) -> Option<&T> {
+        self.items.last()
+    }
+
+    fn is_empty(&self) -> bool {
+        self.items.is_empty()
+    }
+}
+
+fn main() {
+    let mut stack = Stack::new();
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    println!("{}", stack.peek().unwrap());
+    println!("{}", stack.pop().unwrap());
+    println!("{}", stack.pop().unwrap());
+    println!("{}", stack.pop().unwrap());
+    println!("{}", stack.is_empty());
+}
