d1e30276b2a4dcea13bcb9152e113cd8c16c97b4
SwarnabhR <swarnabh.roy@gmail.com>
2024-08-23 22:39:02 +0530

    commit new changes

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,48 @@
-struct Stack<T> {
-    items: Vec<T>,
-}
+mod stack;
 
-impl<T> Stack<T> {
-    fn new() -> Self {
-        Stack { items: Vec::new() }
-    }
-
-    fn push(&mut self, item: T) {
-        self.items.push(item);
-    }
-
-    fn pop(&mut self) -> Option<T> {
-        self.items.pop()
-    }
-
-    fn peek(&self) -> Option<&T> {
-        self.items.last()
-    }
-
-    fn is_empty(&self) -> bool {
-        self.items.is_empty()
-    }
-}
+use stack::Stack;
+use std::io::{ self, Write};
 
 fn main() {
+    loop {
+        println!("Select a data structure:\n");
+        println!("1. Stack\n");
+
+        let mut choice = String::new();
+        io::stdin().read_line(&mut choice).expect("Failed to read line");
+        let choice: u32 = match choice.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match choice {
+            1 => stack_interface(),
+            2 => println!("Linked List selected (not yet implemented)"),
+            3 => {
+                println!("Exiting...");
+                break;
+            }
+            _ => println!("Invalid choice, please try again."),
+        }
+    }
+}
+
+fn stack_interface() {
     let mut stack = Stack::new();
-    stack.push(1);
-    stack.push(2);
-    stack.push(3);
-    println!("{}", stack.peek().unwrap());
-    println!("{}", stack.pop().unwrap());
-    println!("{}", stack.pop().unwrap());
-    println!("{}", stack.pop().unwrap());
-    println!("{}", stack.is_empty());
+
+    loop {
+        println!("Select an operation:\n");
+        println!("1. Push\n");
+        println!("2. Pop\n");
+        println!("3. Peek\n");
+        println!("4. Check if empty\n");
+        println!("5. Exit");
+
+        let mut operation = String::new();
+        io::stdin().read_line(&mut operation ).expect("Failed to read line");
+        let operation: u32 = match operation.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+    };
 }
